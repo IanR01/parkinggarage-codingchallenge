a6c16d6d60e7571d77b68720f5e8d6370fd47f13
IanR01 <ian.r.89@gmail.com>
2023-08-06 15:53:22 +0200

    connect to server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
+
 const express = require('express')
-const port = 3000
+const { Sequelize } = require('sequelize')
+
 const parkingSessionRoutes = require('./routes/parking-session')
 
 // express app
@@ -17,11 +20,24 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/parking-sessions', parkingSessionRoutes)
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// connect to database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Successfully connected to database.')
+  
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`Example app listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+startServer()
